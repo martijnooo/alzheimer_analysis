67caf1c20941dfa29d779c965f791f0dc80a58df
martijnooo <martijnbalder@gmail.com>
2025-04-02 19:31:04 +0530

    Helper functions added for SQL connection

---
+++ helper/sql_engine.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+
+def get_engine():
+    # Prompt the user for username and password
+    username = input("Enter your MySQL username: ")
+    password = input("Enter your MySQL password: ")
+
+    host = '127.0.0.1'
+    port = '3306'
+    bd = "alzheimer"
+
+    # Create an engine to connect to MySQL
+    return create_engine(f'mysql+pymysql://{username}:{password}@{host}:{port}')
